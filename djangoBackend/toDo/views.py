1425c2600c1bf5caef0c6ab466f4672b53da37aa
JulietaR <julietaa.rivero@gmail.com>
2021-10-22 00:02:43 -0300

    Update views.py

--- djangoBackend/toDo/views.py
+++ djangoBackend/toDo/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .serializers import ToDoSerializer
+from rest_framework import viewsets
+from .models import ToDo
 
 # Create your views here.
+
+# View for handling GET and POST requests
+
+class ToDoView(viewsets.ModelViewSet):
+    serializer_class = ToDoSerializer
+    queryset = ToDo.objects.all()
