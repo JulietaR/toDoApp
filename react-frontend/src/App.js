3217da8bdf16d491163a10a2b0d64ddc20469c7b
JulietaR <julietaa.rivero@gmail.com>
2021-10-23 11:32:43 -0300

    Add editTask and modal's function
    Implement a modal for editing tasks and the PUT method

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -6,7 +6,11 @@ import {
   Col, 
   Label, 
   Input, 
-  FormText 
+  FormText,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
 } from 'reactstrap';
 
 class App extends Component {
@@ -16,7 +20,13 @@ class App extends Component {
       data: [],
       loaded: false,
       placeholder: "Loading..",
-      newTask: ''
+      newTask: '',
+      //state property for opening/closing modal
+      modal: false,
+      //state property to hold item of the list id
+      activateItemId: '',
+      //state property to hold item of the list name
+      activateItemName: ''
     };
   }
 
@@ -47,6 +57,7 @@ class App extends Component {
   addTask = () => {
     const newTask = { task: this.state.newTask, completed: false}
     fetch("api/toDos/", {
+      credentials: 'include',
       method: 'POST',
       headers: { 'Content-Type': 'application/json'},
       body: JSON.stringify(newTask)
@@ -54,19 +65,80 @@ class App extends Component {
     this.componentDidMount()
   }
 
+  editTask = (id) => {
+    const newTask = { task: this.state.newTask}
+    fetch("api/toDos/" + id + "/", {
+      credentials: 'include',
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json'},
+      body: JSON.stringify(newTask)
+    })
+    this.componentDidMount()
+    this.closeModal()
+  }
+
+  //Open modal and pass the state of the items in the to Do's list
+  openModal = (toDos) => {
+    this.setState(() => {
+      return { 
+        modal: true,
+        activateItemId: toDos.id,
+        activateItemName: toDos.task
+      };
+    });
+  }
+
+  //Close modal
+  closeModal = () => {
+    this.setState(() => {
+      return { 
+        modal: false
+      };
+    });
+  }
+
   render() {
     return (
       <div>
         <Form>
           <FormText>To-Do List</FormText>
-          {this.state.data.map(toDos => {
+          {this.state.data.map((toDos, index) => {
             return (
               <FormGroup check>
-                <Label key={toDos.id} check>
+                <Label id={toDos.id} key={index} check>
                   <Input type="checkbox" id="checkbox2" />{' '}
                   {toDos.task}
                 </Label>   
-                <a href='#' className='alert-link'>Edit</a>
+                <a 
+                  href='#' 
+                  id={toDos.id}
+                  onClick={() => this.openModal(toDos)}
+                  className='alert-link'
+                >
+                  Edit
+                </a>
+                <Modal 
+                  isOpen={this.state.modal}
+                  itemId={this.state.activateItemId}
+                  itemName={this.state.activateItemName} 
+                  toggle={this.closeModal}
+                >
+                  <ModalHeader>Edit your task: {this.state.activateItemName} </ModalHeader>
+                  <ModalBody>
+                    <Input 
+                      name="edit-input" 
+                      id="edit-input"  
+                      placeholder="Choose a new name"
+                      onChange={this.handleChange} 
+                    />
+                  </ModalBody>
+                  <ModalFooter>
+                    <Button onClick={() => this.editTask(this.state.activateItemId)}>
+                      Save
+                    </Button>
+                    <Button onClick={this.closeModal}>Cancel</Button>
+                  </ModalFooter>
+                </Modal>
               </FormGroup> 
             );
           })}
