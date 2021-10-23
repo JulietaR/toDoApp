0827198bc6a5365f427804d3278603386fd8a38c
JulietaR <julietaa.rivero@gmail.com>
2021-10-22 21:54:03 -0300

    conect API and set UI

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react"
+import { 
+  Button, 
+  Form, 
+  FormGroup, 
+  Col, 
+  Label, 
+  Input, 
+  FormText 
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading.."
+    };
+  }
+
+  async componentDidMount() {
+    fetch("api/toDos")
+      .then(response => {
+        if (response.status > 400) {
+          return this.setState(() => {
+            return { placeholder: "Something went wrong!" };
+          });
+        }
+        return response.json();
+      })
+      .then(data => {
+        this.setState(() => {
+          return {
+            data,
+            loaded: true
+          };
+        });
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <Form>
+          <FormText>To-Do List</FormText>
+          {this.state.data.map(toDos => {
+            return (
+              <FormGroup check>
+                <Label key={toDos.id} check>
+                  <Input type="checkbox" id="checkbox2" />{' '}
+                  {toDos.task}
+                </Label>   
+                <a href='#' className='alert-link'>Edit</a>
+              </FormGroup> 
+            );
+          })}
+          <FormGroup row>
+            <Input name="task-input" id="task-input" sm={10} placeholder="Add a new task" />
+            <Col sm={2}>
+              <Button>Add</Button>
+            </Col>
+          </FormGroup>
+        </Form>
+      </div>
+    )
+  }
 }
 
 export default App;
